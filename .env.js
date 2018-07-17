97cdc0c1bdb4b3a7b4c89364635c08f5d27445fc
Shetia Lamar <butlers@savannahstate.edu>
2018-07-16 20:54:03 -0400

    first version of liri

---
+++ .env.js
@@ -0,0 +1,16 @@
+console.log("We got the keys");
+//Spotify API keys
+
+var client = new Spotify({
+SPOTIFY_ID: process.env.f73655fb0ec4480f82ceaf0d250cf40a,
+SPOTIFY_SECRET: process.env.dad5f08411caebd203abdec49c5
+});
+
+// API keys
+
+var client = new Twitter({
+TWITTER_CONSUMER_KEY: process.env.6VH4hjblclztbJVKaPGZVA0aV,
+TWITTER_CONSUMER_SECRET: process.env.YUAk4VQcZuHNF190OToMr4kpIUxXZEZfb9wUTrlOvyHtNh3RFW,
+TWITTER_ACCESS_TOKEN_KEY: process.env.4726077321-Er447qZ1sagCbbrfAoQxmKxtWtPd4jGX2xdz3DG,
+TWITTER_ACCESS_TOKEN_SECRET: process.env.24KkP90UUmXZTwSzTkDRFmruEG6m3jLk8liVckv5hbqVP
+});
